d8a1d94ee888ff7152b85b711f1b5fa0d487f1a9
Toksik <132383127+T0ks1k24@users.noreply.github.com>
2025-05-29 18:36:25 +0300

    update

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 
+DB_HOST="postgres_database"
+DB_PORT="5432"
 
-echo "Запуск міграцій..."
+echo "Чекаємо доступності бази на $DB_HOST:$DB_PORT..."
+
+while ! nc -z $DB_HOST $DB_PORT; do 
+  echo "База недоступна - чекаємо..."
+  sleep 10
+done
+
+echo "База доступна, запускаємо міграції..."
 python manage.py migrate
 
-echo "Запуск сервера..."
+echo "Запускаємо сервер..."
 exec python manage.py runserver 0.0.0.0:8000
