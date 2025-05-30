149e8069a0ae6c7fc8cf209f2c196222abf27a22
yrkov <yrakovch@gmail.com>
2025-05-30 13:30:26 +0300

    add excel export

--- backend/books/urls.py
+++ backend/books/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import GenreListCreateView, BookListCreateView, BookRetrieveUpdateDestroyView, ImportBooksView
+from .views import GenreListCreateView, BookListCreateView, BookRetrieveUpdateDestroyView, ImportBooksView, ExportBooksExcelView
 
 urlpatterns = [
     path('genres/', GenreListCreateView.as_view(), name='genre-list-create'),
     path('book/', BookListCreateView.as_view(), name='book-list-create'),
     path('book/<int:pk>/', BookRetrieveUpdateDestroyView.as_view(), name='book-detail'),
-    path('import-book/', ImportBooksView.as_view(), name='import-books')
+    path('import-book/', ImportBooksView.as_view(), name='import-books'),
+    path('export/', ExportBooksExcelView.as_view(), name='export-excel'),
 ]
--- backend/books/views.py
+++ backend/books/views.py
@@ -8,6 +8,10 @@ from django_filters.rest_framework import DjangoFilterBackend
 from .serializers import BookSerializer, GenreSerializer, BookDetailSerializer
 from .permissions import IsAdminOrReadOnly
 from parsing.ParsingBook import scrape_books
+import pandas as pd
+from django.http import HttpResponse
+from rest_framework.permissions import IsAuthenticated
+
 
 
 class GenreListCreateView(generics.ListCreateAPIView):
@@ -72,3 +76,50 @@ class ImportBooksView(APIView):
             'updated': count_updated,
             'total': count_new + count_updated
         }, status=status.HTTP_200_OK)
+    
+
+
+class ExportBooksExcelView(APIView):
+    permission_classes = [IsAuthenticated]  
+
+    def get(self, request):
+       
+        title = request.query_params.get('title')
+        genre = request.query_params.get('genre')
+        year = request.query_params.get('year')
+
+        books = Book.objects.select_related('genre').all()
+
+        if title:
+            books = books.filter(title__icontains=title)
+        if genre:
+            books = books.filter(genre__name__icontains=genre)
+        if year:
+            books = books.filter(year=year)
+
+      
+        data = []
+        for book in books:
+            data.append({
+                'ID': book.id,
+                'Назва': book.title,
+                'Жанр': book.genre.name if book.genre else '',
+                'Ціна': float(book.price),
+                'Рейтинг': float(book.rating),
+                'Наявність': book.availability,
+                'Рік': book.year,
+                'Опис': book.description or '',
+                'UPC-код': book.upc_code,
+                'Зображення (URL)': book.image,
+            })
+
+        df = pd.DataFrame(data)
+
+        
+        response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+        response['Content-Disposition'] = 'attachment; filename=books.xlsx'
+
+        with pd.ExcelWriter(response, engine='xlsxwriter') as writer:
+            df.to_excel(writer, index=False, sheet_name='Books')
+
+        return response
