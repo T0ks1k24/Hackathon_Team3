6914a4a6429f676e1c2b970d78ce7188c712a393
Toksik <132383127+T0ks1k24@users.noreply.github.com>
2025-05-30 10:40:26 +0300

    update

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -78,11 +78,11 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+        'NAME': 'dbTeam3',
+        'USER': 'userTeam3',
+        'PASSWORD': 'passworTeam3',
+        'HOST': '3.77.211.196',
+        'PORT': '5430',
     }
 }
 
